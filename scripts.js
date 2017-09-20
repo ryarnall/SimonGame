64650d513653375c656b316832f409871ff9d794
ryarnall <rachelleyarnall@gmail.com>
2017-09-20 14:44:35 -0500

    Correctly displays random sequence of shapes, adding a new one whenever 'test' button is clicked

--- scripts.js
+++ scripts.js
@@ -1,18 +1,50 @@
 $("#testRun").click(lightUp);
 
-
+var simonClicks = [];
+var indexCounter = 0;
 
 function randomShape() {
 	var random = Math.floor(Math.random()*$(".shape").length);
-	console.log("from randomShape:", $(".shape").eq(random));
 	return $(".shape").eq(random);
 }
 
-function lightUp() {
-	var aShape = randomShape();
+function displaySimonClicks() {
+	currentShape = simonClicks[indexCounter];
+	// console.log(currentShape);
 
-	aShape.addClass("buttonActive");
-	setTimeout(function(){ aShape.removeClass("buttonActive"); }, 1000);
+	currentShape.addClass("buttonActive");
+	setTimeout(function() {
+		currentShape.removeClass("buttonActive");
+		indexCounter += 1;
+		if (indexCounter < simonClicks.length) {
+			displaySimonClicks();
+		} else {
+			indexCounter = 0;
+		}
+	}, 1000);
+	
+
+
+}
+
+function lightUp() {
+	var newShape = randomShape();
+	simonClicks.push(newShape);
+
+	displaySimonClicks();
 }
 
 
+
+function checkClicks() {
+	if ($(this).attr("id") == simonClicks[indexCounter]) {
+		console.log("correct");
+		indexCounter += 1;
+		console.log("simonClickedTimes =", simonClickedTimes);	
+	}	else {
+		console.log("nope");
+		simonClicks = [];
+	}
+}
+
+$(".shape").click(checkClicks);
