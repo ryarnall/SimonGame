6628565790177be719ccdca223a2d004482a821d
ryarnall <rachelleyarnall@gmail.com>
2017-09-20 17:44:56 -0500

    Added delay between sequence blinks. Added a couple comments

--- scripts.js
+++ scripts.js
@@ -6,36 +6,39 @@ var indexCounter = 0;
 var currentStreak = 0;
 var	longestStreak = 0;
 
+//returns a random shape from the displayed shapes
 function randomShape() {
 	var random = Math.floor(Math.random()*$(".shape").length);
 	return $(".shape").eq(random);
 }
 
+//causes each shape in the sequence array to "blink" in order
 function displaySimonClicks() {
 	currentShape = simonClicks[indexCounter];
-	// console.log(currentShape);
-
 	currentShape.addClass("buttonActive");
+
 	setTimeout(function() {
 		currentShape.removeClass("buttonActive");
 		indexCounter += 1;
+
 		if (indexCounter < simonClicks.length) {
-			displaySimonClicks();
+			setTimeout(displaySimonClicks, 500);
 		} else {
 			indexCounter = 0;
 		}
 	}, 1000);
 }
 
+//adds a new random shape to the sequence then calls function displaySimonClicks
 function addToSequence() {
 	var newShape = randomShape();
 	simonClicks.push(newShape);
 
 	indexCounter = 0;
-	displaySimonClicks();
-	
+	displaySimonClicks();	
 }
 
+//updates the streak counters' displays
 function updateStreaks() {	
 	if (currentStreak > longestStreak) {
 		longestStreak = currentStreak;		
@@ -44,19 +47,19 @@ function updateStreaks() {
 	$("#longestStreak").text(longestStreak);
 }
 
-
+//checks if the user clicked space matches the next in the sequence
 function checkClicks() {
 	var waitingShape = simonClicks[indexCounter].attr("id");
+
 	if ($(this).attr("id") == waitingShape) {
 		console.log("correct");
 		indexCounter += 1;
-
 		if (indexCounter >= simonClicks.length) {
 			currentStreak = simonClicks.length;
 			updateStreaks();
-
 			setTimeout(addToSequence, 1000);
 		}	
+
 	}	else {
 		console.log("nope");
 		simonClicks = [];
@@ -65,4 +68,5 @@ function checkClicks() {
 	}
 }
 
+//sets the click handler for the shapes
 $(".shape").on("click", checkClicks);
